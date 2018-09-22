41125863fba3db273ffe81825d9cbd9794b3be91
Lingfei Gao <hob19930414@gmail.com>
2018-09-22 18:09:18 -0400

    Initial Commit

---
+++ TDSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TDSample
+//
+//  Created by Lingfei Gao on 2018/9/22.
+//  Copyright © 2018年 Lingfei Gao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
